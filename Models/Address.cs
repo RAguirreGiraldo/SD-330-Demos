c2927621e89df202affb8245f6920b7f2340952c
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-03-06 23:50:55 -0600

    update class

--- Models/Address.cs
+++ Models/Address.cs
@@ -2,5 +2,15 @@
 {
     public class Address
     {
+        public int Id { get; set; } 
+
+        public string AddressLine1 { get; set; }
+
+        public string?  AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string CountryRegion { get; set; }
+
+        public virtual HashSet<Customer> Customers { get; set; }=new HashSet<Customer?>();
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,16 @@
-﻿namespace SD_330_Demos.Models
+﻿using System.Collections.Generic;
+
+namespace SD_330_Demos.Models
 {
     public class Customer
     {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Phone { get; set; }
+        public virtual HashSet<Address> Addresses { get; set; } = new HashSet<Address>();
+
+
     }
 }
